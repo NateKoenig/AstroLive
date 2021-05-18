25d81ae5f529754ba66f67477898bdd17875d2d4
Nate Koenig <55851808+NateKoenig@users.noreply.github.com>
2021-05-18 17:03:34 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 // #AstroLive Web Application
 // Nate Koenig, May 18
 //
-
+// Astro data taken from https://www.nasa.gov/astronauts/biographies/active
 
 
 //TODO: API stuff (fetch info from website that we make / or find one online)
@@ -14,24 +14,29 @@
 // Set scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
+radius = 35;
 
 // Set rendering
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// Create planet and set camera position
-const geometry = new THREE.SphereGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const sphere = new THREE.Mesh( geometry, material );
-scene.add( sphere );
 
-camera.position.z = 5;
+// Create planet given radius
+function planet(r) {
+	const geometry = new THREE.SphereGeometry(r, 100, 100);
+	const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	const sphere = new THREE.Mesh( geometry, material );
+	scene.add( sphere );
+}
 
 // Set controls
-const controls = new THREE.OrbitControls( camera, renderer.domElement );
-controls.autoRotate = true;
-controls.update();
+function controls() {
+	const controls = new THREE.OrbitControls( camera, renderer.domElement );
+	controls.autoRotate = true;
+	controls.update();
+}
 
 // Animation / actual running of project
 const animate = function () {
@@ -45,5 +50,9 @@ const animate = function () {
 	renderer.render( scene, camera );
 }
 
-// Run the project
-animate();
+// Driver
+function init() {
+	planet(radius);
+	controls();
+	animate();
+}
